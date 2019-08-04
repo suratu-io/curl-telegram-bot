01805ef375155c5baa8de422823dd66d05044a27
Alikhan Arshabekov <alihan.arshabekov@gmail.com>
2019-08-04 18:26:49 +0600

    feat: add base architecture

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+    "github.com/go-telegram-bot-api/telegram-bot-api"
     "github.com/olehan/kek"
     "os"
 )
