cef5255624c9c303b2dd97d2c16e661e3164e7ae
Alikhan Arshabekov <alihan.arshabekov@gmail.com>
2019-08-05 16:38:31 +0600

    feat: add config package

---
+++ app/config/model.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+    "github.com/suratu-io/curl-telegram-bot/app/logger"
+    "os"
+    "strconv"
+)
+
+const (
+    BotApiTokenEnv    = "BOT_API_TOKEN"
+    UpdateTimeoutEnv  = "UPDATE_TIMEOUT"
+    ProductionModeEnv = "PRODUCTION_MODE"
+)
+
+var (
+    log = logger.Factory.NewLogger("config")
+
+    BotApiToken    = os.Getenv(BotApiTokenEnv)
+    ProductionMode = os.Getenv(ProductionModeEnv) == "true"
+    UpdateTimeout  = 60
+)
+
+func init() {
+    ut, err := strconv.ParseInt(os.Getenv(UpdateTimeoutEnv), 10, 32)
+    if err != nil {
+        log.Warn.Println("update timeout has not been set properly, using the default")
+    } else {
+        UpdateTimeout = int(ut)
+    }
+}
